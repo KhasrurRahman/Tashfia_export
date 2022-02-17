e482715e8025933f53506ea49de42089acdab1d1
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2022-02-17 12:50:04 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,6 @@
 use App\Http\Controllers\Api\ApiAuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::group([
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-// ], function ($router) {
-//     Route::post('/login', [ApiAuthController::class, 'login']);
-//    Route::post('/register', [ApiAuthController::class, 'register']);
-//     Route::post('/logout', [ApiAuthController::class, 'logout']);
-//     Route::post('/refresh', [ApiAuthController::class, 'refresh']);
-//     Route::get('/user-profile', [ApiAuthController::class, 'userProfile']);
-// });
-
-
-
 Route::post('login', 'Api\ApiAuthController@login');
 
 Route::group(['middleware' => ['jwt.verify']], function () {
