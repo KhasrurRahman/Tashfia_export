7bbaa8eb16db8ef2b943366080f8eab33cdddff6
Md. Khasrur Rahman <khasrur8@gmail.com>
2022-03-12 02:09:59 +0600

    Update header.blade.php

--- resources/views/layouts/backend/partial/include/header.blade.php
+++ resources/views/layouts/backend/partial/include/header.blade.php
@@ -13,31 +13,7 @@
                 <button type="button" id="navbar-fullscreen" class="nav-link"><i class="ik ik-maximize"></i></button>
             </div>
             <div class="top-menu d-flex align-items-center">
-                <div class="dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="notiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                                class="ik ik-bell"></i><span class="badge bg-danger">3</span></a>
-                    <div class="dropdown-menu dropdown-menu-right notification-dropdown" aria-labelledby="notiDropdown">
-                        <h4 class="header">{{ __('Notifications')}}</h4>
-                        <div class="notifications-wrap">
-                            <a href="#" class="media">
-                                <span class="d-flex">
-                                    <i class="ik ik-check"></i>
-                                </span>
-                                <span class="media-body">
-                                    <span class="heading-font-family media-heading">{{ __('Invitation accepted')}}</span>
-                                    <span class="media-content">{{ __('Your have been Invited ...')}}</span>
-                                </span>
-                            </a>
-                            <a href="#" class="media">
-                                <span class="media-body">
-                                    <span class="heading-font-family media-heading">{{ __('Steve Smith')}}</span>
-                                    <span class="media-content">{{ __('I slowly updated projects')}}</span>
-                                </span>
-                            </a>
-                        </div>
-                        <div class="footer"><a href="javascript:void(0);">{{ __('See all activity')}}</a></div>
-                    </div>
-                </div>
+
 
                 <div class="dropdown">
                     <a class="dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="avatar"
